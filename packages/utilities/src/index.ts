e837b5b80e5ef3eec5cc61d2f8aff13aab747171
Zachary Weidenbach <zweidenbach@gmail.com>
2025-04-02 23:49:14 -0700

    fix exports, simplify ci

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/ChimericAsyncRead/MetaAggregatorFactory.js';
 export * from './lib/ChimericPromise/ChimericPromiseFactory.js';
+export * from './lib/ChimericPromise/IdiomaticPromiseFactory.js';
+export * from './lib/ChimericPromise/ReactivePromiseFactory.js';
